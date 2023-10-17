03b7b98f4b10ec5cbc6941832c72c07190a15577
Inge Amlien <iamlien@gmail.com>
2023-10-17 13:20:56 +0000

    Fix new token url Add minimal error handling

--- tsd_adapter.sh
+++ tsd_adapter.sh
@@ -13,24 +13,37 @@ TOKEN_STRING=$( jq -n \
                   --arg sec "$TSD_SECRET" \
                   '{id: $id, secret_challenge: $sec}' )
 
-TOKEN=$(curl "https://data.tsd.usit.no/capability_token/" \
+# New token url...
+REPLY=$(curl "https://data.tsd.usit.no/all/auth/instances/token" \
     -X POST \
     -H 'Content-Type: application/json' \
-    -d "${TOKEN_STRING}" | jq -r .token)
+    -d "${TOKEN_STRING}")
 
+hastoken=$(jq 'has("token")'  <<< ${REPLY})
+
+if [[ ! "$hastoken" == "true" ]] ; then
+    echo "Token not found: ${REPLY}"
+    exit 1
+fi
+
+TOKEN=$(jq -r .token <<< ${REPLY})
 
 TSD_GROUP=$(jwtd ${TOKEN} | jq -r .groups[0])
 TSD_PROJECT=$(jwtd ${TOKEN} | jq -r .proj)
 TSD_PATH=$(jwtd ${TOKEN} | jq -r .path)
 
-# echo "TOKEN: ${TOKEN}"
-# echo "TSD_GROUP: ${TSD_GROUP}"
-# echo "TSD_PROJECT: ${TSD_PROJECT}"
-# echo "TSD_PATH: ${TSD_PATH}"
+echo "XXXXXXXX"
+echo "${TOKEN_STRING}"
+echo "${TOKEN}"
+echo "${TSD_GROUP}"
+echo "${TSD_PROJECT}"
+echo "${TSD_PATH}"
+echo "XXXXXXXX"
+
 
 curl -X PUT \
     -H 'Accept: application/json' \
     -H "Authorization: Bearer ${TOKEN}" \
     --data-binary "@${INPUTFILE}" \
-    -v "https://data.tsd.usit.no/v1/${TSD_PROJECT}/files/stream/${TSD_GROUP}/${TSD_PATH}/${INPUTFILE}"
+    -v "https://data.tsd.usit.no/${TSD_PROJECT}/files/stream/${TSD_GROUP}/${TSD_PATH}/${INPUTFILE}"
 
